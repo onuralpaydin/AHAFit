12a207815f5388cdbec91b1d50c45f16fccf794b
Hüseyin Yılmaz <91hyilmaz@gmail.com>
2022-02-23 10:46:03 +0300

    Düzeltmeler.

--- AHAFit-UI/HomeForm.Designer.cs
+++ AHAFit-UI/HomeForm.Designer.cs
@@ -59,9 +59,9 @@ namespace AHAFit_UI
             // 
             // btnReports
             // 
-            this.btnReports.Location = new System.Drawing.Point(108, 90);
+            this.btnReports.Location = new System.Drawing.Point(93, 78);
             this.btnReports.Name = "btnReports";
-            this.btnReports.Size = new System.Drawing.Size(184, 80);
+            this.btnReports.Size = new System.Drawing.Size(158, 69);
             this.btnReports.TabIndex = 0;
             this.btnReports.Text = "Reports";
             this.btnReports.UseVisualStyleBackColor = true;
@@ -69,9 +69,9 @@ namespace AHAFit_UI
             // 
             // btnHistory
             // 
-            this.btnHistory.Location = new System.Drawing.Point(373, 90);
+            this.btnHistory.Location = new System.Drawing.Point(320, 78);
             this.btnHistory.Name = "btnHistory";
-            this.btnHistory.Size = new System.Drawing.Size(184, 80);
+            this.btnHistory.Size = new System.Drawing.Size(158, 69);
             this.btnHistory.TabIndex = 0;
             this.btnHistory.Text = "History";
             this.btnHistory.UseVisualStyleBackColor = true;
@@ -79,9 +79,9 @@ namespace AHAFit_UI
             // 
             // btnMemberComp
             // 
-            this.btnMemberComp.Location = new System.Drawing.Point(638, 90);
+            this.btnMemberComp.Location = new System.Drawing.Point(547, 78);
             this.btnMemberComp.Name = "btnMemberComp";
-            this.btnMemberComp.Size = new System.Drawing.Size(184, 80);
+            this.btnMemberComp.Size = new System.Drawing.Size(158, 69);
             this.btnMemberComp.TabIndex = 0;
             this.btnMemberComp.Text = "Member Comparisons";
             this.btnMemberComp.UseVisualStyleBackColor = true;
@@ -89,9 +89,9 @@ namespace AHAFit_UI
             // 
             // btnFoodSt
             // 
-            this.btnFoodSt.Location = new System.Drawing.Point(903, 90);
+            this.btnFoodSt.Location = new System.Drawing.Point(774, 78);
             this.btnFoodSt.Name = "btnFoodSt";
-            this.btnFoodSt.Size = new System.Drawing.Size(184, 80);
+            this.btnFoodSt.Size = new System.Drawing.Size(158, 69);
             this.btnFoodSt.TabIndex = 0;
             this.btnFoodSt.Text = "Food Statistics";
             this.btnFoodSt.UseVisualStyleBackColor = true;
@@ -99,9 +99,9 @@ namespace AHAFit_UI
             // 
             // btnBMI
             // 
-            this.btnBMI.Location = new System.Drawing.Point(1168, 90);
+            this.btnBMI.Location = new System.Drawing.Point(1001, 78);
             this.btnBMI.Name = "btnBMI";
-            this.btnBMI.Size = new System.Drawing.Size(184, 80);
+            this.btnBMI.Size = new System.Drawing.Size(158, 69);
             this.btnBMI.TabIndex = 0;
             this.btnBMI.Text = "BMI Calculator";
             this.btnBMI.UseVisualStyleBackColor = true;
@@ -110,123 +110,123 @@ namespace AHAFit_UI
             // dtpHomeDate
             // 
             this.dtpHomeDate.Checked = false;
-            this.dtpHomeDate.Location = new System.Drawing.Point(12, 222);
+            this.dtpHomeDate.Location = new System.Drawing.Point(45, 203);
             this.dtpHomeDate.Name = "dtpHomeDate";
-            this.dtpHomeDate.Size = new System.Drawing.Size(445, 35);
+            this.dtpHomeDate.Size = new System.Drawing.Size(382, 31);
             this.dtpHomeDate.TabIndex = 1;
             this.dtpHomeDate.Value = new System.DateTime(2022, 2, 22, 0, 0, 0, 0);
             this.dtpHomeDate.ValueChanged += new System.EventHandler(this.dtpHomeDate_ValueChanged);
             // 
             // lblSumCalorie
             // 
-            this.lblSumCalorie.Location = new System.Drawing.Point(63, 432);
+            this.lblSumCalorie.Location = new System.Drawing.Point(54, 372);
             this.lblSumCalorie.Name = "lblSumCalorie";
-            this.lblSumCalorie.Size = new System.Drawing.Size(202, 97);
+            this.lblSumCalorie.Size = new System.Drawing.Size(173, 84);
             this.lblSumCalorie.TabIndex = 2;
             this.lblSumCalorie.Text = "1500 Calories \r\nEaten Today ";
             // 
             // lblRemainCalorie
             // 
-            this.lblRemainCalorie.Location = new System.Drawing.Point(324, 418);
+            this.lblRemainCalorie.Location = new System.Drawing.Point(278, 360);
             this.lblRemainCalorie.Name = "lblRemainCalorie";
-            this.lblRemainCalorie.Size = new System.Drawing.Size(188, 111);
+            this.lblRemainCalorie.Size = new System.Drawing.Size(161, 96);
             this.lblRemainCalorie.TabIndex = 2;
             this.lblRemainCalorie.Text = "1000 Calories \r\nyou need to take for today";
             // 
             // lblRemainWater
             // 
-            this.lblRemainWater.Location = new System.Drawing.Point(576, 418);
+            this.lblRemainWater.Location = new System.Drawing.Point(494, 360);
             this.lblRemainWater.Name = "lblRemainWater";
-            this.lblRemainWater.Size = new System.Drawing.Size(154, 140);
+            this.lblRemainWater.Size = new System.Drawing.Size(132, 121);
             this.lblRemainWater.TabIndex = 2;
             this.lblRemainWater.Text = "You should drink 4 more glasses of water today.";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(124, 724);
+            this.label4.Location = new System.Drawing.Point(106, 624);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(158, 29);
+            this.label4.Size = new System.Drawing.Size(142, 25);
             this.label4.TabIndex = 2;
             this.label4.Text = "Carbohydrate";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(368, 724);
+            this.label5.Location = new System.Drawing.Point(315, 624);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(90, 29);
+            this.label5.Size = new System.Drawing.Size(80, 25);
             this.label5.TabIndex = 2;
             this.label5.Text = "Protein";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(551, 724);
+            this.label6.Location = new System.Drawing.Point(472, 624);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(47, 29);
+            this.label6.Size = new System.Drawing.Size(43, 25);
             this.label6.TabIndex = 2;
             this.label6.Text = "Fat";
             // 
             // lblCarbo
             // 
             this.lblCarbo.AutoSize = true;
-            this.lblCarbo.Location = new System.Drawing.Point(152, 775);
+            this.lblCarbo.Location = new System.Drawing.Point(130, 668);
             this.lblCarbo.Name = "lblCarbo";
-            this.lblCarbo.Size = new System.Drawing.Size(98, 29);
+            this.lblCarbo.Size = new System.Drawing.Size(90, 25);
             this.lblCarbo.TabIndex = 2;
             this.lblCarbo.Text = "245/500";
             // 
             // lblPro
             // 
             this.lblPro.AutoSize = true;
-            this.lblPro.Location = new System.Drawing.Point(368, 766);
+            this.lblPro.Location = new System.Drawing.Point(315, 660);
             this.lblPro.Name = "lblPro";
-            this.lblPro.Size = new System.Drawing.Size(72, 29);
+            this.lblPro.Size = new System.Drawing.Size(66, 25);
             this.lblPro.TabIndex = 2;
             this.lblPro.Text = "44/85";
             // 
             // lblFat
             // 
             this.lblFat.AutoSize = true;
-            this.lblFat.Location = new System.Drawing.Point(535, 769);
+            this.lblFat.Location = new System.Drawing.Point(459, 663);
             this.lblFat.Name = "lblFat";
-            this.lblFat.Size = new System.Drawing.Size(72, 29);
+            this.lblFat.Size = new System.Drawing.Size(66, 25);
             this.lblFat.TabIndex = 2;
             this.lblFat.Text = "55/60";
             // 
             // btnWater
             // 
-            this.btnWater.Location = new System.Drawing.Point(832, 198);
+            this.btnWater.Location = new System.Drawing.Point(713, 171);
             this.btnWater.Name = "btnWater";
-            this.btnWater.Size = new System.Drawing.Size(160, 87);
+            this.btnWater.Size = new System.Drawing.Size(137, 75);
             this.btnWater.TabIndex = 3;
             this.btnWater.Text = "+1 Glass of Water";
             this.btnWater.UseVisualStyleBackColor = true;
             // 
             // btnAteSomething
             // 
-            this.btnAteSomething.Location = new System.Drawing.Point(1022, 198);
+            this.btnAteSomething.Location = new System.Drawing.Point(876, 171);
             this.btnAteSomething.Name = "btnAteSomething";
-            this.btnAteSomething.Size = new System.Drawing.Size(160, 87);
+            this.btnAteSomething.Size = new System.Drawing.Size(137, 75);
             this.btnAteSomething.TabIndex = 3;
             this.btnAteSomething.Text = "I ate something";
             this.btnAteSomething.UseVisualStyleBackColor = true;
             // 
             // btnProgramChanger
             // 
-            this.btnProgramChanger.Location = new System.Drawing.Point(1215, 198);
+            this.btnProgramChanger.Location = new System.Drawing.Point(1041, 171);
             this.btnProgramChanger.Name = "btnProgramChanger";
-            this.btnProgramChanger.Size = new System.Drawing.Size(160, 87);
+            this.btnProgramChanger.Size = new System.Drawing.Size(137, 75);
             this.btnProgramChanger.TabIndex = 3;
             this.btnProgramChanger.Text = "Change My Program";
             this.btnProgramChanger.UseVisualStyleBackColor = true;
             // 
             // btnProfileUpdate
             // 
-            this.btnProfileUpdate.Location = new System.Drawing.Point(1409, 198);
+            this.btnProfileUpdate.Location = new System.Drawing.Point(1208, 171);
             this.btnProfileUpdate.Name = "btnProfileUpdate";
-            this.btnProfileUpdate.Size = new System.Drawing.Size(160, 87);
+            this.btnProfileUpdate.Size = new System.Drawing.Size(137, 75);
             this.btnProfileUpdate.TabIndex = 3;
             this.btnProfileUpdate.Text = "Profile Update";
             this.btnProfileUpdate.UseVisualStyleBackColor = true;
@@ -234,61 +234,61 @@ namespace AHAFit_UI
             // dgvDailyFoodList
             // 
             this.dgvDailyFoodList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvDailyFoodList.Location = new System.Drawing.Point(829, 374);
+            this.dgvDailyFoodList.Location = new System.Drawing.Point(711, 322);
             this.dgvDailyFoodList.Name = "dgvDailyFoodList";
             this.dgvDailyFoodList.RowHeadersWidth = 92;
             this.dgvDailyFoodList.RowTemplate.Height = 37;
-            this.dgvDailyFoodList.Size = new System.Drawing.Size(740, 817);
+            this.dgvDailyFoodList.Size = new System.Drawing.Size(634, 704);
             this.dgvDailyFoodList.TabIndex = 4;
             // 
             // cmbMeal
             // 
             this.cmbMeal.FormattingEnabled = true;
-            this.cmbMeal.Location = new System.Drawing.Point(1378, 315);
+            this.cmbMeal.Location = new System.Drawing.Point(1181, 272);
             this.cmbMeal.Name = "cmbMeal";
-            this.cmbMeal.Size = new System.Drawing.Size(191, 37);
+            this.cmbMeal.Size = new System.Drawing.Size(164, 33);
             this.cmbMeal.TabIndex = 5;
             // 
             // label11
             // 
             this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(1273, 318);
+            this.label11.Location = new System.Drawing.Point(1091, 274);
             this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(68, 29);
+            this.label11.Size = new System.Drawing.Size(60, 25);
             this.label11.TabIndex = 2;
             this.label11.Text = "Filter";
             // 
             // cmbMealSumCalorie
             // 
             this.cmbMealSumCalorie.FormattingEnabled = true;
-            this.cmbMealSumCalorie.Location = new System.Drawing.Point(64, 994);
+            this.cmbMealSumCalorie.Location = new System.Drawing.Point(55, 857);
             this.cmbMealSumCalorie.Name = "cmbMealSumCalorie";
-            this.cmbMealSumCalorie.Size = new System.Drawing.Size(191, 37);
+            this.cmbMealSumCalorie.Size = new System.Drawing.Size(164, 33);
             this.cmbMealSumCalorie.TabIndex = 5;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(63, 949);
+            this.label1.Location = new System.Drawing.Point(54, 818);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(416, 29);
+            this.label1.Size = new System.Drawing.Size(373, 25);
             this.label1.TabIndex = 6;
             this.label1.Text = "Today\'s total calories based on meals";
             // 
             // lblMealSumCalorie
             // 
             this.lblMealSumCalorie.AutoSize = true;
-            this.lblMealSumCalorie.Location = new System.Drawing.Point(277, 1002);
+            this.lblMealSumCalorie.Location = new System.Drawing.Point(237, 864);
             this.lblMealSumCalorie.Name = "lblMealSumCalorie";
-            this.lblMealSumCalorie.Size = new System.Drawing.Size(65, 29);
+            this.lblMealSumCalorie.Size = new System.Drawing.Size(60, 25);
             this.lblMealSumCalorie.TabIndex = 2;
             this.lblMealSumCalorie.Text = "2500";
             // 
             // HomeForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1581, 1203);
+            this.ClientSize = new System.Drawing.Size(1355, 1037);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.cmbMealSumCalorie);
             this.Controls.Add(this.cmbMeal);
