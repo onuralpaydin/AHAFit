4a16d18633f4181120691a0c535f7c31549f5105
Hüseyin Yılmaz <91hyilmaz@gmail.com>
2022-03-03 14:23:05 +0300

    Member Comparisons sayfasına başlandı.

--- AHAFit-UI/MemberCompForm.Designer.cs
+++ AHAFit-UI/MemberCompForm.Designer.cs
@@ -29,12 +29,33 @@ namespace AHAFit_UI
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(265, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // MemberCompForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Name = "MemberCompForm";
             this.Text = "MemberCompForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
