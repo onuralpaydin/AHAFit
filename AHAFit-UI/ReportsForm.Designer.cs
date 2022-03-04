901909d43ddca67fbeabb3463682642ad1555a63
alperakdemir <alperakdemir004@gmail.com>
2022-03-05 01:17:18 +0300

    Reports Form Eklendir

--- AHAFit-UI/ReportsForm.Designer.cs
+++ AHAFit-UI/ReportsForm.Designer.cs
@@ -29,12 +29,198 @@ namespace AHAFit_UI
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblTotalCalorie = new System.Windows.Forms.Label();
+            this.lblCalorie = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cmbCategories = new System.Windows.Forms.ComboBox();
+            this.cmbMeals = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rdbMonthly = new System.Windows.Forms.RadioButton();
+            this.rdbWeekly = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.lblTotalCalorie);
+            this.groupBox1.Controls.Add(this.lblCalorie);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.cmbCategories);
+            this.groupBox1.Controls.Add(this.cmbMeals);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.rdbMonthly);
+            this.groupBox1.Controls.Add(this.rdbWeekly);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(647, 282);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Benchmark Reports";
+            // 
+            // lblTotalCalorie
+            // 
+            this.lblTotalCalorie.AutoSize = true;
+            this.lblTotalCalorie.Location = new System.Drawing.Point(540, 84);
+            this.lblTotalCalorie.Name = "lblTotalCalorie";
+            this.lblTotalCalorie.Size = new System.Drawing.Size(63, 13);
+            this.lblTotalCalorie.TabIndex = 9;
+            this.lblTotalCalorie.Text = "TotalCalorie";
+            // 
+            // lblCalorie
+            // 
+            this.lblCalorie.AutoSize = true;
+            this.lblCalorie.Location = new System.Drawing.Point(431, 84);
+            this.lblCalorie.Name = "lblCalorie";
+            this.lblCalorie.Size = new System.Drawing.Size(39, 13);
+            this.lblCalorie.TabIndex = 8;
+            this.lblCalorie.Text = "Calorie";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(313, 84);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(88, 13);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Average Calorie :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(539, 37);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(64, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "All Members";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(421, 37);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Member";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(580, 37);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 2;
+            // 
+            // cmbCategories
+            // 
+            this.cmbCategories.FormattingEnabled = true;
+            this.cmbCategories.Location = new System.Drawing.Point(70, 139);
+            this.cmbCategories.Name = "cmbCategories";
+            this.cmbCategories.Size = new System.Drawing.Size(157, 21);
+            this.cmbCategories.TabIndex = 5;
+            this.cmbCategories.SelectedIndexChanged += new System.EventHandler(this.cmbCategories_SelectedIndexChanged);
+            // 
+            // cmbMeals
+            // 
+            this.cmbMeals.FormattingEnabled = true;
+            this.cmbMeals.Location = new System.Drawing.Point(70, 84);
+            this.cmbMeals.Name = "cmbMeals";
+            this.cmbMeals.Size = new System.Drawing.Size(157, 21);
+            this.cmbMeals.TabIndex = 4;
+            this.cmbMeals.SelectedIndexChanged += new System.EventHandler(this.cmbMeals_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Categories :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Meals : ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Select Timespan :";
+            // 
+            // rdbMonthly
+            // 
+            this.rdbMonthly.AutoSize = true;
+            this.rdbMonthly.Location = new System.Drawing.Point(165, 37);
+            this.rdbMonthly.Name = "rdbMonthly";
+            this.rdbMonthly.Size = new System.Drawing.Size(62, 17);
+            this.rdbMonthly.TabIndex = 1;
+            this.rdbMonthly.TabStop = true;
+            this.rdbMonthly.Text = "Monthly";
+            this.rdbMonthly.UseVisualStyleBackColor = true;
+            // 
+            // rdbWeekly
+            // 
+            this.rdbWeekly.AutoSize = true;
+            this.rdbWeekly.Location = new System.Drawing.Point(98, 37);
+            this.rdbWeekly.Name = "rdbWeekly";
+            this.rdbWeekly.Size = new System.Drawing.Size(61, 17);
+            this.rdbWeekly.TabIndex = 0;
+            this.rdbWeekly.TabStop = true;
+            this.rdbWeekly.Text = "Weekly";
+            this.rdbWeekly.UseVisualStyleBackColor = true;
+            // 
+            // ReportsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(671, 306);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "ReportsForm";
             this.Text = "ReportsForm";
+            this.Load += new System.EventHandler(this.ReportsForm_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton rdbMonthly;
+        private System.Windows.Forms.RadioButton rdbWeekly;
+        private System.Windows.Forms.ComboBox cmbCategories;
+        private System.Windows.Forms.ComboBox cmbMeals;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblCalorie;
+        private System.Windows.Forms.Label lblTotalCalorie;
     }
 }
