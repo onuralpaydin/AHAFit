88d841799c502fbc9719808c623f2496c24d6361
Onur Alp Aydın <o.a.aydin17@gmail.com>
2022-03-05 04:02:05 +0300

    foodStatistics

--- AHAFit-BLL/Onur.cs
+++ AHAFit-BLL/Onur.cs
@@ -10,7 +10,26 @@ namespace AHAFit_BLL
 {
     public class Onur
     {
+        //Context db = new Context();
+        //MemberFood memberFood = new MemberFood();
+        //Food food = new Food();
+      
   
-        
+        //public List<Food>MostEatenFood(int memberId)
+        //{
+        //    List<MemberFood> memberFoodList = new List<MemberFood>();
+        //    List<Food> foodList = new List<Food>();
+        //    memberFood.MemberId = memberId;
+
+        //    memberFoodList = db.MembersFoods.OrderBy(x => x.FoodId).ToList();
+        //    foreach (var item in memberFoodList)
+        //    {
+        //        foodList.Add(item.);
+        //    }
+
+        //    return foodList;
+         
+            
+        }
     }
 }
